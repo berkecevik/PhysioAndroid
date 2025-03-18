374ed4f6f152756f3782e11274093f008c718188
berkecevik <berkecevik99@gmail.com>
2025-03-18 08:59:36 +0100

    first commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,81 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("de.undercouch.download")
+}
+
+android {
+    namespace = "com.example.physiobuddy"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.example.physiobuddy"
+        minSdk = 24
+        targetSdk = 30
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
+    }
+}
+
+// Import DownloadMPTasks task
+project.extra["ASSET_DIR"] = "${projectDir}/src/main/assets"
+apply(from = "download_tasks.gradle")
+
+dependencies {
+    // Kotlin lang
+    implementation("androidx.core:core-ktx:1.8.0")
+
+    // App compat and UI dependencies
+    implementation("androidx.appcompat:appcompat:1.5.1")
+    implementation("com.google.android.material:material:1.7.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.fragment:fragment-ktx:1.5.4")
+
+    // Navigation library
+    val nav_version = "2.5.3"
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
+
+    // CameraX core library
+    val camerax_version = "1.2.0-alpha02"
+    implementation("androidx.camera:camera-core:$camerax_version")
+    implementation("androidx.camera:camera-camera2:$camerax_version")
+    implementation("androidx.camera:camera-lifecycle:$camerax_version")
+    implementation("androidx.camera:camera-view:$camerax_version")
+
+    // WindowManager
+    implementation("androidx.window:window:1.1.0-alpha03")
+
+    // Unit testing
+    testImplementation("junit:junit:4.13.2")
+
+    // Instrumented testing
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+
+    // MediaPipe Library
+    implementation("com.google.mediapipe:tasks-vision:0.10.14")
+}
